3e8e67562b0d895b25cf49f1a166c3ae92c105e6
Ramon van Koll <ramonvankoll@hotmail.com>
2017-05-02 16:56:46 +0200

    Change in the menu and buttons. Video with background color.

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,20 @@
+    'use strict';
+
+    /**
+    * @ngdoc function
+    * @name codeGorillaApp.controller:MainCtrl
+    * @description
+    * # MainCtrl
+    * Controller of the codeGorillaApp
+    */
+
+    angular.module('codeGorillaApp')
+           .controller('LoginCtrl', function () {
+    this.submit = function(user)
+    {
+    /**
+    *console.log(user);
+    */
+
+    };
+    });
